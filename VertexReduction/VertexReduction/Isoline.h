c18cb3605b309cdc2ec8b7beb9fe8fed4ecccac2
LAPTOP-3KH1TLTK\Lalo <idv14c.cortiz@uartesdigitales.edu.mx>
2017-06-25 15:53:02 -0500

    corrección en los comentarios.

--- VertexReduction/VertexReduction/Isoline.h
+++ VertexReduction/VertexReduction/Isoline.h
@@ -3,16 +3,38 @@
 
 #include <vector>
 
-//!Declaration of CIsoline Class
+
+/**
+* @brief Isoline class. stores a vector of Vertices.
+*/
 class CIsoline
 {
 public:
+
+	/************************************************************************/
+	/*                            Constructor & Destructor.                 */
+	/************************************************************************/
+
 	//!Default constructor and destructor.
 	CIsoline();
 	~CIsoline();
 
+	/************************************************************************/
+	/*                       Class' functions                               */
+	/************************************************************************/
+	
+	/**
+	 * @brief Initialize function allocates if needed, memory for it's variables.
+	 */
 	void Initialize();
+	/**
+	* @brief Releases memory, and deletes it's variables.
+	*/
 	void Destroy();
+
+	/************************************************************************/
+	/*						  Member Variables.								*/
+	/************************************************************************/
 
 	//!Array to store the lines.
 	std::vector<CVertex> m_VertexVector;
