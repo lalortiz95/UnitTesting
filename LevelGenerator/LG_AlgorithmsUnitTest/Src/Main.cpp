3a266466b272beadf8117d06a1f4f77aacf6b817
LAPTOP-3KH1TLTK\Lalo <idv14c.cortiz@uartesdigitales.edu.mx>
2017-07-05 21:57:44 -0500

    Cambio de nombre del proyecto, y la sln

---
+++ LevelGenerator/LG_AlgorithmsUnitTest/Src/Main.cpp
@@ -0,0 +1,19 @@
+#pragma once
+#include <LG_MarchingSquare.h>
+#include <time.h>
+#include <gtest/gtest.h>
+
+//using namespace LevelGenerator;
+//TEST(AlgorithmUnitTest, Test1)
+//{
+//	srand(5);
+//	LG_MarchingSquare MarchingSquare;
+//	MarchingSquare.Run();
+//	//EXPECT_TRUE();
+//}
+
+int main(/*int argc, char **argv*/)
+{
+	//::testing::InitGoogleTest(&argc, argv);
+	return 0/*RUN_ALL_TESTS()*/;
+}
