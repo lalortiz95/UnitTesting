94417cf9bfec6b0d7a6190e1e60341a38c8555ed
LAPTOP-3KH1TLTK\Lalo <idv14c.cortiz@uartesdigitales.edu.mx>
2017-07-09 16:12:52 -0500

    Se agregaron funciones y comentarios a la clase RDP.

--- LevelGenerator/LG_Algorithms/Include/LG_RDP.h
+++ LevelGenerator/LG_Algorithms/Include/LG_RDP.h
@@ -5,8 +5,8 @@
 namespace LevelGenerator
 {
 	/**
-	*	@brief This class generates Ramer-Douglas-Peucker Algorithm.
-	*/
+	 *	@brief This class generates Ramer-Douglas-Peucker Algorithm.
+	 */
 	class LG_ALGORITHMS_EXPORT LG_RDP
 	{
 	public:
@@ -34,8 +34,32 @@ namespace LevelGenerator
 		LG_Isoline m_OriginalIsoline;
 
 		/**
-		*	@brief The reduced isoline.
-		*/
+		 *	@brief The reduced isoline.
+		 */
 		LG_Isoline m_FinalIsoline;
+
+		/**
+		 *	@brief The range to reduce nodes.
+		 */
+		float m_fRange;
+
+		///************************************************************************/
+		///*						   Class Functions.							  */
+		///************************************************************************/
+
+		/**
+		 *  @brief This function initialize all variables of the class.
+		 */
+		void Init();
+
+		/**
+		 *	@brief This function free the memory of the class, and destroys it's variables.
+		 */
+		void Destroy();
+
+		/**
+		 *	@brief This function is the only one you need to generate Ramer-Douglas-Peucker algorithm.
+		 */
+		void Run();
 	};
 }
--- LevelGenerator/LG_Algorithms/Src/LG_RDP.cpp
+++ LevelGenerator/LG_Algorithms/Src/LG_RDP.cpp
@@ -2,12 +2,33 @@
 
 namespace LevelGenerator
 {
+	//! Default constructor.
 	LG_RDP::LG_RDP()
 	{
+
 	}
 
-
+	//! Default destructor.
 	LG_RDP::~LG_RDP()
 	{
+		Destroy();
+	}
+
+	//! This function initialize all variables of the class.
+	void LG_RDP::Init()
+	{
+
+	}
+
+	//! This function free the memory of the class, and destroys it's variables.
+	void LG_RDP::Destroy()
+	{
+
+	}
+
+	//! This function is the only one you need to generate Ramer-Douglas-Peucker algorithm.
+	void LG_RDP::Run()
+	{
+
 	}
 }
