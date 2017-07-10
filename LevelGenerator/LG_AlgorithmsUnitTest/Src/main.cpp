e5ac6c9b64babdf3640064a811a26c7dbf69f30a
DESKTOP-3DJC0VB\carlo <idv15a.ciniguez@uartesdigitales.edu.mx>
2017-07-10 16:26:47 -0500

    Main arreglado

---
+++ LevelGenerator/LG_AlgorithmsUnitTest/Src/main.cpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <LG_Grid.h>
+#include <time.h>
+
+//using namespace LevelGenerator;
+//TEST(AlgorithmUnitTest, Test1)
+//{
+//	srand(5);
+//	LG_MarchingSquare MarchingSquare;
+//	MarchingSquare.Run();
+//	//EXPECT_TRUE();
+//}
+
+int main(/*int argc, char **argv*/)
+{
+	//::testing::InitGoogleTest(&argc, argv);
+	return 0/*RUN_ALL_TESTS()*/;
+}
