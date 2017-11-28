154f3177c905fc246d6f47295303a6a87a0f74e6
LAPTOP-3KH1TLTK\Lalo <idv14c.cortiz@uartesdigitales.edu.mx>
2017-11-27 18:11:37 -0600

    Se agregó el archivo de C# que faltaba, para exportar dll de C#

---
+++ LevelGenerator/LG_CSWrapper/LG_CSWrap.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+
+namespace LG_CSWrapper
+{
+    struct LG_Vector3D
+    {
+        float X, Y, Z;
+    }
+
+    public class LG_CSWrap
+    {
+        // From c++ Dll (unmanaged)
+        [DllImport("LG_Algorithms", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void GenerateRooms(int iRoomAmount);
+    }
+}
